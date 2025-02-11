40ece211e5acb63f110b0cddedb9b4fa099f8b85
James Douglass <jdouglass@stanford.edu>
2025-02-10 16:27:17 -0800

    Commenting out WIP code.

--- src/ckanext-natcap/ckanext/natcap/assets/js/natcap-dataset-clip.js
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-dataset-clip.js
@@ -4,15 +4,15 @@ this.ckan.module('natcap-dataset-clip', function($, _) {
       dataset: "",
     }
 
-    _onClick: function () {
-      // ideally: show the clipper in a modal
-      // For now, just open a new tab with the correct URLs.
+    //_onClick: function () {
+    //  // ideally: show the clipper in a modal
+    //  // For now, just open a new tab with the correct URLs.
 
-    }
+    //}
 
     initialize: function () {
       this.clipButton = this.$('.icon-button-clip');
-      this.clipButton.on('click', $.proxy(this._onClick, this));
+      //this.clipButton.on('click', $.proxy(this._onClick, this));
     }
   };
 });
