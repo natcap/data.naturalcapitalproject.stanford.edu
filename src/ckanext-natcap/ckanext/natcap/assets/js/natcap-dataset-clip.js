bb5bc0d6be6121da0980c9354dd5d3a13076cc0d
James Douglass <jdouglass@stanford.edu>
2025-02-12 14:47:55 -0800

    Simplifying query and selecting first match.

--- src/ckanext-natcap/ckanext/natcap/assets/js/natcap-dataset-clip.js
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-dataset-clip.js
@@ -11,17 +11,19 @@ this.ckan.module('natcap-dataset-clip', function($, _) {
       var endpoint = 'https://clipping-service-897938321824.us-west1.run.app';
       var url = `${endpoint}/clip?cog_url=${this.options.dataset}`;
 
-      var lat = this.$('div.mappreview').attr('center-lat');
+      var map = this.$('div.mappreview')[0];
+
+      var lat = map.attr('center-lat');
       if (lat !== undefined) {
         url += `&lat=${lat}`;
       }
 
-      var lng = this.$('div.mappreview').attr('center-lng');
+      var lng = map.attr('center-lng');
       if (lng !== undefined) {
         url += `&lng=${lng}`;
       }
 
-      var zoom = this.$('div.mappreview').attr('zoom');
+      var zoom = map.attr('zoom');
       if (zoom !== undefined) {
         url += `&zoom=${zoom}`;
       }
