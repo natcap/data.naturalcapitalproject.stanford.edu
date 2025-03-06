a943b1ebc537b0aec01db83b229689b5016ab7a2
James Douglass <jdouglass@stanford.edu>
2025-03-06 14:56:02 -0800

    Aha!  Fixing syntax error - plugin=object.
    RE: #57

--- src/ckanext-natcap/ckanext/natcap/assets/js/natcap-text-linkify.js
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-text-linkify.js
@@ -4,27 +4,27 @@ ckan.module("natcap-text-linkify", function($, _) {
   return {
     options: {
       debug: false,
+    },
+
+    // Adapted from https://stackoverflow.com/a/49634926
+    initialize: function () {
+      const textDiv = $(this.el);
+      console.log(textDiv);
+      var replaceText = textDiv.innerText;
+
+      //URLs starting with http://, https://, or ftp://
+      var replacePattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+      replaceText.replace(replacePattern '<a href="$1" target="_blank">$1</a>');
+
+      //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+      replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+      replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+      //Change email addresses to mailto:: links.
+      replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+      replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+      $(this.el).html(replaceText);
     }
-  },
-
-  // Adapted from https://stackoverflow.com/a/49634926
-  initialize: function () {
-    const textDiv = $(this.el);
-    console.log(textDiv);
-    var replaceText = textDiv.innerText;
-
-    //URLs starting with http://, https://, or ftp://
-    var replacePattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replaceText.replace(replacePattern '<a href="$1" target="_blank">$1</a>');
-
-    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-
-    //Change email addresses to mailto:: links.
-    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-
-    $(this.el).html(replaceText);
   }
 });
