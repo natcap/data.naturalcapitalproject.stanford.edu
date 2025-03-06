982634bbe6b5473eea325784e166650286bc8842
James Douglass <jdouglass@stanford.edu>
2025-03-06 15:11:06 -0800

    Forgot to define variables with var. RE:#57

--- src/ckanext-natcap/ckanext/natcap/assets/js/natcap-text-linkify.js
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-text-linkify.js
@@ -17,11 +17,11 @@ ckan.module("natcap-text-linkify", function($, _) {
       replacedText.replace(replacePattern, '<a href="$1" target="_blank">$1</a>');
 
       //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-      replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+      var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
       replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
       //Change email addresses to mailto:: links.
-      replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+      var replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
       replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
       $(this.el).html(replaceText);
