a9e74aae3bb854838bb0b705b17d3eab259280c8
Claire Simpson <cesimpson@stanford.edu>
2025-07-14 18:20:14 -0600

    Access center_lat_lon stored in ckan dataset extras from mappreview js (#83)

--- src/ckanext-mappreview/ckanext/mappreview/plugin.py
+++ src/ckanext-mappreview/ckanext/mappreview/plugin.py
@@ -24,11 +24,49 @@ def should_show(pkg):
     return get_mappreview(pkg)
 
 
+# def parse_metadata(pkg):
+#     mappreview = get_mappreview(pkg)
+#     if not mappreview:
+#         return {}
+#     return json.loads(mappreview['value'])
+
 def parse_metadata(pkg):
     mappreview = get_mappreview(pkg)
     if not mappreview:
         return {}
-    return json.loads(mappreview['value'])
+
+    metadata = json.loads(mappreview['value'])
+
+    # Extract dataset-level extras
+    center_lat_lon = None
+    # bbox_75 = None
+    # if pkg.get('center_lat_lon'):
+    #     try:
+    #         center_lat_lon = json.loads(pkg['center_lat_lon'])
+    #     except json.JSONDecodeError:
+    #         pass
+
+    for extra in pkg.get('extras', []):
+        if extra['key'] == 'center_lat_lon':
+            try:
+                center_lat_lon = json.loads(extra['value'])
+            except json.JSONDecodeError:
+                pass
+        # if extra['key'] == 'bbox_75':
+        #     try:
+        #         bbox_75 = json.loads(extra['value'])
+        #     except json.JSONDecodeError:
+        #         pass
+
+    # Inject into each vector layer (optional: only if one layer?)
+    for layer in metadata.get('layers', []):
+        if layer.get('type') == 'vector':
+            if center_lat_lon:
+                layer['center_lat_lon'] = center_lat_lon
+            # if bbox_75:
+            #     layer['bbox_75'] = bbox_75
+
+    return metadata
 
 
 def get_layer_js(layer):
