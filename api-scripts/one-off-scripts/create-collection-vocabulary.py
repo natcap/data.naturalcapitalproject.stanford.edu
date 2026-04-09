3849d00af245f589a4c5616e7a20a8e9d7c0a9f3
Megan Nissel <megnissel@gmail.com>
2026-04-09 16:46:55 -0400

    helper script for creating the collection vocabulary with an initial set of tags

---
+++ api-scripts/one-off-scripts/create-collection-vocabulary.py
@@ -0,0 +1,45 @@
+"""A script used for creating the `collection` Tag Vocabulary on CKAN.
+
+Includes a list of TAGS to create within the `collection` vocabulary. These were
+extracted from the existing datasets' "extras" on the production Hub on 2025-07-11.
+"""
+import os
+import requests
+
+import ckanapi.errors
+from ckanapi import RemoteCKAN
+
+
+DEV_APIKEY = os.environ.get('CKAN_LOCAL_APIKEY')
+DEV_CKAN_URL = "https://localhost:8443"
+STAGING_APIKEY = os.environ.get('CKAN_STAGING_APIKEY')
+STAGING_CKAN_URL = "https://data-staging.naturalcapitalproject.org"
+PROD_APIKEY = os.environ.get('CKAN_APIKEY')
+PROD_CKAN_URL = "https://data.naturalcapitalproject.stanford.edu"
+
+TAGS = [
+    {'name': 'FOOTPRINT IMPACT TOOL'},
+    {'name': 'PEOPLE PLANET PROSPERITY COLOMBIA'},
+    {'name': 'PEOPLE PLANET PROSPERITY COOK ISLANDS'},
+    {'name': 'PEOPLE PLANET PROSPERITY PHILIPPINES'},
+    {'name': 'ESA CCI LULC'},
+    {'name': 'ESA WORLDCOVER LULC'},
+    {'name': 'GLC_FCS30D LULC'},
+    {'name': 'KOPPEN-GEIGER CLIMATE ZONES'},
+    {'name': 'KOPPEN-GEIGER CLIMATE ZONES FUTURE'},
+    {'name': 'KOPPEN-GEIGER CLIMATE ZONES HISTORIC'},
+]
+
+
+def vocab_create(ckan_apikey, ckan_url, vocab_name, vocab_tags):
+
+    session = requests.Session()
+    session.headers.update({'Authorization': ckan_apikey})
+    session.verify = False
+
+    with RemoteCKAN(ckan_url, apikey=ckan_apikey, session=session) as catalog: 
+        catalog.action.vocabulary_create(name=vocab_name, tags=vocab_tags)
+
+
+if __name__ == "__main__":
+    vocab_create(DEV_APIKEY, DEV_CKAN_URL, "collection", TAGS)
